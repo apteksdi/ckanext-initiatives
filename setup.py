33b29e87815b7f5a97d8e279666a7ca7a62fa9a1
Grahame Bowland <grahame.bowland@qcif.edu.au>
2020-06-22 13:20:46 +0800

    release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-initiatives',
-    version='0.0.2',
+    version='0.0.4',
     description='',
     license='GPL3',
     author='Grahame Bowland, QCIF',
